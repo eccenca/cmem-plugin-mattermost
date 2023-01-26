ca12d60d9dd766870d6e2506e4cd435f1ed32453
Hannes Hartmann <hanne.hartmann@eccenca.com>
2023-01-26 14:28:17 +0100

    changed code to more methods to make it cleaner and to get a higher performance

--- cmem_plugin_mattermost/workflow/__init__.py
+++ cmem_plugin_mattermost/workflow/__init__.py
@@ -1,6 +1,13 @@
 """A Mattermost integration Plugin"""
+from typing import Sequence
+
 import requests
+from cmem_plugin_base.dataintegration.context import ExecutionContext
 from cmem_plugin_base.dataintegration.description import Plugin, PluginParameter
+from cmem_plugin_base.dataintegration.entity import Entities
+from cmem_plugin_base.dataintegration.parameter.multiline import (
+    MultilineStringParameterType,
+)
 from cmem_plugin_base.dataintegration.plugins import WorkflowPlugin
 
 
@@ -27,20 +34,17 @@ _ 'message' = the message you want to send
             name="url",
             label="URL",
             description="The url of the mattermost server you want to use.",
-            default_value="https://chat.eccenca.com",
         ),
         PluginParameter(
             name="access_token",
             label="Access Token",
             description="The access token of the bot,"
             "which was given when the bot was created.",
-            default_value="Need to fill",
         ),
         PluginParameter(
             name="bot_name",
             label="Bot name",
             description="The name or id of the bot you will send with.",
-            default_value="Need to be filled for user message.",
         ),
         PluginParameter(
             name="user",
@@ -49,19 +53,20 @@ _ 'message' = the message you want to send
             " nickname or email of the user you want to get the message."
             " If you want to send your message to multiple"
             " user separate them with a semicolon ';'.",
-            default_value="Need to be filled for user message.",
+            default_value="",
         ),
         PluginParameter(
             name="channel",
             label="Channel name",
             description="The name or id of the channel you want to get the message.",
-            default_value="Need to be filled for channel message.",
+            default_value="",
         ),
         PluginParameter(
             name="message",
             label="Message",
             description="Words, which together will be a message :-)",
-            default_value="Type messages here.",
+            param_type=MultilineStringParameterType(),
+            default_value="",
         ),
     ],
 )
@@ -86,49 +91,70 @@ class MattermostPlugin(WorkflowPlugin):
         self.channel = channel
         self.message = message
 
-        send_message_with_bot_to_user(self)
-        send_message_with_bot_to_channel(self)
 
+    def execute(self, inputs: Sequence[Entities], context: ExecutionContext) -> None:
+        self.send_message_with_bot_to_user()
+        self.send_message_with_bot_to_channel()
 
-def send_message_with_bot_to_user(self):
-    """sends messages from bot to user."""
-
-    headers = {
-        "Authorization": f"Bearer {self.access_token}",
-    }
-    # Request to find the bot ID with the bot name
-    response = requests.get(
-        f"{self.url}/api/v4/bots",
-        headers=headers,
-        timeout=5,
-    )
-    i = 0
-    bot_id = None
-    for _ in response.json():
-        if self.bot_name in (
-            response.json()[i]["username"],
-            response.json()[i]["display_name"],
-        ):
-            bot_id = response.json()[i]["user_id"]
-            break
-        i += 1
-
-    user_list = self.user.split(sep=";")
-    i = 0
-    for _ in user_list:
-        user_request = user_list[i].lstrip()
-        # Request to find the user ID with the username
-        response = requests.post(
-            f"{self.url}/api/v4/users/search",
+    def get_bot_id(self):
+        """Request to find the bot ID with the bot name"""
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+        }
+        response = requests.get(
+            f"{self.url}/api/v4/bots",
             headers=headers,
-            json={"term": user_request},
+            json={"per_page": 1},
             timeout=5,
         )
-        if response.status_code == 200 and response.json() != []:
-            user_id = response.json()[0]["id"]
 
+        bot_id = str
+        list_bot_entities = response.json()
+        for _ in list_bot_entities:
+            if self.bot_name in (
+                _["username"],
+                _["display_name"],
+            ):
+                bot_id = _["user_id"]
+                break
+        return bot_id
+
+    def get_user_id_list(self) -> list:
+        """Request to find the user ID with the username.
+            Returns a list of id`s not a string."""
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+        }
+        response = requests.get(
+            f"{self.url}/api/v4/users?per_page=200",
+            headers=headers,
+            timeout=20,
+        )
+        list_userentities = response.json()
+        list_username = self.user.split(sep=";")
+        user_id = []
+        for _ in list_username:
+            username = _.lstrip()
+            for _ in list_userentities:
+                if username in (
+                    _["username"],
+                    _["email"],
+                    _["nickname"],
+                    (_["first_name"] + " " + _["last_name"]),
+                ):
+                    user_id.append(_["id"])
+        return user_id
+
+    def send_message_with_bot_to_user(self) -> None:
+        """sends messages from bot to one or more users."""
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+        }
+        list_user_id = self.get_user_id_list()
+        bot_id = self.get_bot_id()
+        for _ in list_user_id:
             # payload for json to generate a direct channel with post request
-            data = [bot_id, user_id]
+            data = [bot_id, _]
             # post request to generate the direct channel
             response = requests.post(
                 f"{self.url}/api/v4/channels/direct",
@@ -146,33 +172,37 @@ def send_message_with_bot_to_user(self):
             requests.post(
                 f"{self.url}/api/v4/posts", headers=headers, json=payload, timeout=5
             )
-        i += 1
 
+    def get_channel_id(self):
+        """Request to find the channel ID with the bot name"""
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+        }
 
-def send_message_with_bot_to_channel(self):
-    """sends messages from bot to channel."""
+        # generate a channel_id
+        response = requests.get(
+            f"{self.url}/api/v4/channels",
+            headers=headers,
+            timeout=5,
+        )
+        list_channel = response.json()
+        channel_id = str
+        for _ in list_channel:
+            if self.channel in (
+                _["display_name"],
+                _["name"],
+            ):
+                channel_id = _["id"]
+                break
+        return channel_id
 
-    headers = {
-        "Authorization": f"Bearer {self.access_token}",
-    }
-
-    # generate a channel_id
-    response = requests.get(
-        f"{self.url}/api/v4/channels",
-        headers=headers,
-        timeout=5,
-    )
-    i = 0
-    channel_id = None
-    for _ in response.json():
-        if self.channel in (
-            response.json()[i]["display_name"],
-            response.json()[i]["name"],
-        ):
-            channel_id = response.json()[i]["id"]
-            break
-        i += 1
-    # payload
-    data = {"channel_id": channel_id, "message": self.message}
-    # Post request for the message
-    requests.post(f"{self.url}/api/v4/posts", headers=headers, json=data, timeout=5)
+    def send_message_with_bot_to_channel(self) -> None:
+        """sends messages from bot to channel."""
+        headers = {
+            "Authorization": f"Bearer {self.access_token}",
+        }
+        channel_id = self.get_channel_id()
+        # payload
+        data = {"channel_id": channel_id, "message": self.message}
+        # Post request for the message
+        requests.post(f"{self.url}/api/v4/posts", headers=headers, json=data, timeout=5)
