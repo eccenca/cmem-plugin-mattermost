6fa9a2745263f33beceb2b26f7ca09199982e869
Hannes Hartmann <hanne.hartmann@eccenca.com>
2023-02-14 22:20:29 +0100

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,13 +8,12 @@ from urllib3.util.retry import Retry
 @pytest.fixture
 def mattermost_service(module_scoped_container_getter) -> str:
     """Wait for the api from mattermost to become responsive"""
-    service = module_scoped_container_getter.get("mattermost").network_info[0]
-
-    base_url = f"http://{service.hostname}:{service.host_port}"
 
     retry = Retry(total=5, backoff_factor=3, status_forcelist=[500, 502, 503, 504])
     session = Session()
     session.mount("http://", HTTPAdapter(max_retries=retry))
+    service = module_scoped_container_getter.get("mattermost").network_info[0]
+    base_url = f"http://{service.hostname}:{service.host_port}"
 
     assert session.get(f"{base_url}/health_check").ok
     return base_url
