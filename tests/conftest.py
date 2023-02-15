d4cb685b20d4b27437edb68e2dad4dfc3104b11b
Hannes Hartmann <hanne.hartmann@eccenca.com>
2023-02-15 07:15:31 +0100

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 # Invoking this fixture: 'function_scoped_container_getter' starts all services
 import pytest
-from requests import Session
+import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
@@ -10,7 +10,7 @@ def mattermost_service(module_scoped_container_getter) -> str:
     """Wait for the api from mattermost to become responsive"""
 
     retry = Retry(total=5, backoff_factor=3, status_forcelist=[500, 502, 503, 504])
-    session = Session()
+    session = requests.Session()
     session.mount("http://", HTTPAdapter(max_retries=retry))
     service = module_scoped_container_getter.get("mattermost").network_info[0]
     base_url = f"http://{service.hostname}:{service.host_port}"
