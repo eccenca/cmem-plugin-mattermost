6055b31a66deb108457f69512658d7c641c10a27
saipraneeth <2506664+msaipraneeth@users.noreply.github.com>
2024-03-19 06:56:07 +0000

    add annotation for test methods

--- tests/test_mattermost.py
+++ tests/test_mattermost.py
@@ -57,7 +57,8 @@ def get_entities(sample_data) -> Entities:
 
 
 @needs_cmem
-def test_execute_with_inputs_and_static(mattermost_service):
+def test_execute_with_inputs_and_static(mattermost_service: str) -> None:
+    """Test executing with inputs and static"""
     MattermostPlugin(
         mattermost_service,
         access_token,
@@ -68,7 +69,8 @@ def test_execute_with_inputs_and_static(mattermost_service):
     ).execute([get_entities(sample_data_full)], TestExecutionContext())
 
 
-def test_execute_error(mattermost_service):
+def test_execute_error(mattermost_service: str) -> None:
+    """Test execute error with out user or channel"""
     with pytest.raises(ValueError):
         MattermostPlugin(
             mattermost_service,
@@ -81,7 +83,8 @@ def test_execute_error(mattermost_service):
 
 
 @needs_cmem
-def test_execute_with_inputs_only(mattermost_service):
+def test_execute_with_inputs_only(mattermost_service: str) -> None:
+    """Test execute with inputs only"""
     MattermostPlugin(
         mattermost_service,
         access_token,
@@ -93,7 +96,8 @@ def test_execute_with_inputs_only(mattermost_service):
 
 
 @needs_cmem
-def test_execute_with_no_inputs(mattermost_service):
+def test_execute_with_no_inputs(mattermost_service: str) -> None:
+    """Test execute with no inputs"""
     MattermostPlugin(
         mattermost_service,
         access_token,
@@ -104,7 +108,8 @@ def test_execute_with_no_inputs(mattermost_service):
     ).execute([], TestExecutionContext())
 
 
-def test_execute_with_empty_message_error(mattermost_service):
+def test_execute_with_empty_message_error(mattermost_service: str) -> None:
+    """Test execute with empty message"""
     with pytest.raises(ValueError):
         MattermostPlugin(
             mattermost_service,
@@ -116,7 +121,8 @@ def test_execute_with_empty_message_error(mattermost_service):
         ).send_message_to_provided_parameter()
 
 
-def test_send_message_with_bot_to_user(mattermost_service):
+def test_send_message_with_bot_to_user(mattermost_service: str) -> None:
+    """Test send message to user with bot"""
     MattermostPlugin(
         mattermost_service,
         access_token,
@@ -127,13 +133,15 @@ def test_send_message_with_bot_to_user(mattermost_service):
     ).send_message_with_bot_to_user()
 
 
-def test_send_message_with_bot_to_channel(mattermost_service):
+def test_send_message_with_bot_to_channel(mattermost_service: str) -> None:
+    """Test sending message to channel with bot"""
     MattermostPlugin(
         mattermost_service, access_token, bot_name, "", channel, "Channel test message"
     ).send_message_with_bot_to_channel()
 
 
-def test_send_message_with_bot_to_channel_error(mattermost_service):
+def test_send_message_with_bot_to_channel_error(mattermost_service: str) -> None:
+    """Test send_message_with_bot_to_channel error"""
     with pytest.raises(ValueError):
         wrong_channel = "wrong_channel_name"
         MattermostPlugin(
@@ -146,7 +154,8 @@ def test_send_message_with_bot_to_channel_error(mattermost_service):
         ).send_message_with_bot_to_channel()
 
 
-def test_get_user_id(mattermost_service):
+def test_get_user_id(mattermost_service: str) -> None:
+    """Test get_user_id method"""
     assert (
         MattermostPlugin(mattermost_service, access_token, bot_name, user, channel, message).get_id(
             user
@@ -155,7 +164,8 @@ def test_get_user_id(mattermost_service):
     )
 
 
-def test_get_user_id_error(mattermost_service):
+def test_get_user_id_error(mattermost_service: str) -> None:
+    """Test get_user_id error"""
     with pytest.raises(ValueError):
         user_empty = ""
         MattermostPlugin(
@@ -168,7 +178,8 @@ def test_get_user_id_error(mattermost_service):
         ).get_id(user_empty)
 
 
-def test_get_channel_id(mattermost_service):
+def test_get_channel_id(mattermost_service: str) -> None:
+    """Test getting channel"""
     assert (
         MattermostPlugin(
             mattermost_service, access_token, bot_name, user, channel, message
@@ -177,7 +188,8 @@ def test_get_channel_id(mattermost_service):
     )
 
 
-def test_get_channel_id_error(mattermost_service):
+def test_get_channel_id_error(mattermost_service: str) -> None:
+    """Test get channel id error"""
     with pytest.raises(ValueError):
         channel_wrong = ""
         MattermostPlugin(
@@ -185,7 +197,8 @@ def test_get_channel_id_error(mattermost_service):
         ).get_channel_id()
 
 
-def test_get_bot_id_error(mattermost_service):
+def test_get_bot_id_error(mattermost_service: str) -> None:
+    """Test get_bot_id error"""
     with pytest.raises(ValueError):
         bot_name_wrong = "wrong_bot"
         MattermostPlugin(
@@ -193,7 +206,8 @@ def test_get_bot_id_error(mattermost_service):
         ).get_id(bot_name_wrong)
 
 
-def test_header():
+def test_header() -> None:
+    """Test header"""
     result = header(access_token)
     assert result == {
         "Authorization": f"Bearer {access_token.decrypt()}",
@@ -201,19 +215,22 @@ def test_header():
     }
 
 
-def test_get_request_handler(mattermost_service):
+def test_get_request_handler(mattermost_service: str) -> None:
+    """Test get request handler"""
     result = get_request_handler(url, "users", access_token)
     assert result.ok
 
 
-def test_send_message_to_provided_parameter_error(mattermost_service):
+def test_send_message_to_provided_parameter_error(mattermost_service: str) -> None:
+    """Test send message to"""
     with pytest.raises(ValueError):
         MattermostPlugin(
             mattermost_service, access_token, bot_name, user, channel, ""
         ).send_message_to_provided_parameter()
 
 
-def test_send_message_to_provided_parameter(mattermost_service):
+def test_send_message_to_provided_parameter(mattermost_service: str) -> None:
+    """Test send message to provided chanel or user"""
     MattermostPlugin(
         mattermost_service, access_token, bot_name, user, channel, message
     ).send_message_to_provided_parameter()
@@ -225,19 +242,22 @@ def test_send_message_to_provided_parameter(mattermost_service):
     ).send_message_to_provided_parameter()
 
 
-def test_get_dataset(mattermost_service):
+def test_get_dataset(mattermost_service: str) -> None:
+    """Test get_dataset"""
     result = get_dataset(url, "users", access_token, [user])
     assert result[0]["username"] == f"{user}"
 
 
 @needs_cmem
-def test_autocomplete_error():
+def test_autocomplete_error() -> None:
+    """Test autocomplete_error"""
     with pytest.raises(ValueError):
         MattermostSearch("users", "username").autocomplete(["cmem"], [], TestPluginContext())
 
 
 @needs_cmem
-def test_autocomplete():
+def test_autocomplete() -> None:
+    """Test autocomplete"""
     MattermostSearch("users", "username").autocomplete(
         query_terms=["cmem"],
         depend_on_parameter_values=[url, access_token],
